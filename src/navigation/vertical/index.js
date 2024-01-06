0e6f984be724496b38f07ac363a553c132c63ca9
Loredana Cirstea <loredana.cirstea@gmail.com>
2024-01-06 20:30:35 +0100

    remove sponsors

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -22,41 +22,41 @@ function processMenu() {
       icon: 'https://ping.pub/logo.svg',
     })
   }
-  chainMenus.push({ header: 'sponsors' })
-  chainMenus.push({
-    title: 'Becole',
-    href: 'https://becole.com',
-    logo: 'https://becole.com/static/logo/logo_becole.png',
-  })
-  chainMenus.push({
-    title: 'Osmosis',
-    href: 'https://app.osmosis.zone',
-    logo: 'https://ping.pub/logos/osmosis.jpg',
-  })
+  // chainMenus.push({ header: 'sponsors' })
+  // chainMenus.push({
+  //   title: 'Becole',
+  //   href: 'https://becole.com',
+  //   logo: 'https://becole.com/static/logo/logo_becole.png',
+  // })
+  // chainMenus.push({
+  //   title: 'Osmosis',
+  //   href: 'https://app.osmosis.zone',
+  //   logo: 'https://ping.pub/logos/osmosis.jpg',
+  // })
   chainMenus.push({ header: 'LINKS' })
-  if (isTestnet()) {
-    chainMenus.push({
-      title: 'Mainnet Explorer',
-      href: 'https://ping.pub',
-      icon: 'ChromeIcon',
-    })
-  } else {
-    chainMenus.push({
-      title: 'Testnet Explorer',
-      href: 'http://testnet.ping.pub',
-      icon: 'LifeBuoyIcon',
-    })
-  }
-  chainMenus.push({
-    title: 'Twitter',
-    href: 'https://twitter.com/ping_pub',
-    icon: 'TwitterIcon',
-  })
-  chainMenus.push({
-    title: 'FAQ',
-    href: 'https://github.com/ping-pub/explorer/discussions',
-    icon: 'MessageSquareIcon',
-  })
+  // if (isTestnet()) {
+  //   chainMenus.push({
+  //     title: 'Mainnet Explorer',
+  //     href: 'https://ping.pub',
+  //     icon: 'ChromeIcon',
+  //   })
+  // } else {
+  //   chainMenus.push({
+  //     title: 'Testnet Explorer',
+  //     href: 'http://testnet.ping.pub',
+  //     icon: 'LifeBuoyIcon',
+  //   })
+  // }
+  // chainMenus.push({
+  //   title: 'Twitter',
+  //   href: 'https://twitter.com/ping_pub',
+  //   icon: 'TwitterIcon',
+  // })
+  // chainMenus.push({
+  //   title: 'FAQ',
+  //   href: 'https://github.com/ping-pub/explorer/discussions',
+  //   icon: 'MessageSquareIcon',
+  // })
   chainMenus.push({
     title: 'Github',
     href: 'https://github.com/ping-pub/explorer',
